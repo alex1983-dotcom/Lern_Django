cd6ca27928f9789e0f3f55e348b1c2702570f2ab
Макаров Александр <alebarda2018@gmail.com>
2024-12-08 01:59:49 +0300

    Тренировка 1 - изучение метода HttpResponse

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    html_content = "<html><body><h1>Hello, world. You're at the polls index.</h1></body></html>"
+    return HttpResponse(html_content, content_type='text/html')
