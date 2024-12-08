53157fbcf92d27f48ab2eaf43a6f068dc596411d
Макаров Александр <alebarda2018@gmail.com>
2024-12-09 02:15:04 +0300

    Появилась ошибка CORB, устранить пока неудалось

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Post
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
 
 
 def post_list(request):
