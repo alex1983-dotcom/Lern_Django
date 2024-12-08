9ec383151ae3374e926ff17294a187b6594f1670
Макаров Александр <alebarda2018@gmail.com>
2024-12-08 12:18:14 +0300

    Обновление базы данных

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'MyWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'MySite',
+        'USER': 'postgres',
+        'PASSWORD': 'cng_777_anna',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
