6305217f0be7e6e53641f5904b2da74b857087a6
Макаров Александр <alebarda2018@gmail.com>
2024-12-08 17:54:39 +0300

    Создал функции post_list, post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-
-# Create your views here.
-# blog/views.py
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the blog index.")
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post,
+                         id=id,
+                         status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+
