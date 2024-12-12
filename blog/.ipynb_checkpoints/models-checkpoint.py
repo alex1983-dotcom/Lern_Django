888966237d42524cf336092e791143da1d093b92
Макаров Александр <alebarda2018@gmail.com>
2024-12-12 17:59:35 +0300

    Неудавшаяся версия

--- blog/.ipynb_checkpoints/models-checkpoint.py
+++ blog/.ipynb_checkpoints/models-checkpoint.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import reverse
-from django.utils.text import slugify
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -17,13 +16,17 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish', blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
     objects = models.Manager()  # Менеджер применяемый по умолчанию
     published = PublishedManager()  # Конкретно - прикладной менеджер
 
@@ -36,14 +39,3 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day,
-                             self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,45 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+import markdown
+
 
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+
+    for post in posts:
+        # Добавим вывод для отладки
+        print(f"Post ID: {post.id}, Title: {post.title}, Slug: {post.slug}")
+        print(f"Generated URL: {post.get_absolute_url()}")
+
+    transformed_posts = []
+    for post in posts:
+        transformed_post = Post(
+            id=post.id,
+            title=post.title,
+            body=markdown.markdown(post.body),
+            publish=post.publish,
+            author=post.author,
+            status=post.status
+        )
+        transformed_posts.append(transformed_post)
+
+    return render(request, 'blog/post/list.html', {'posts': transformed_posts})
 
 
-def post_detail(request, id):
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                         id=id,
-                         status=Post.Status.PUBLISHED)
+                             slug=post,
+                             status=Post.Status.PUBLISHED,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+
+    # Преобразование текста из Markdown в HTML
+    post.body = markdown.markdown(post.body)
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
