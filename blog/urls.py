d269eac19af5e70c6f5f3d2aff46d015d57677f0
Макаров Александр <alebarda2018@gmail.com>
2024-12-15 23:16:04 +0300

    Изменения в urls.py0

--- blog/urls.py
+++ blog/urls.py
@@ -1,19 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from .feeds import LatestPostsFeed
-
 
 app_name = "blog"
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
-    path('tag/<slug:tag_slug>/',
-         views.post_list, name='post_list_by_tag'),
-    path("<int:year>/<int:month>/<int:day>/<slug:post>/",
-         views.post_detail, name="post_detail"),
-    path("<int:post_id>/share/",
-         views.post_share, name="post_share"),
-    path("<int:post_id>/comment/",
-         views.post_comment, name='post_comment'),
+    re_path(r'^tag/(?P<tag_slug>[-\wа-яА-Я]+)/$', views.post_list, name='post_list_by_tag'),
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name="post_detail"),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
+    path("<int:post_id>/comment/", views.post_comment, name='post_comment'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
